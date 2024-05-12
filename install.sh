ee86c89bd955e080dc88c9f68b8acf360576d8b3
unknown <diego.collao.albornoz@gmail.com>
2024-05-11 23:52:59 -0400

    dirsearch bloodhound powershell dotnet6.0 java neo4j

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 sudo apt update
 sudo apt upgrade -y
 
+# Timg show image files in Terminal 
+# timg file.png
 sudo apt install timg -y
 
 ########################VSCODE################################
@@ -34,6 +36,56 @@ rm -rf rclone-current-linux-amd64.zip
 #rclone config 
 ##############################################################
 
+##########################Bloodhound###########################
+# Installing JAVA (requirements)
+sudo apt install openjdk-11-jdk -y
+sudo apt install neo4j -y
+# Start neo4j service
+sudo neo4j console
+# Configure neo4j (user:password neo4j:neo4j)
+# Put new password (required)
+cd ~/Downloads
+wget https://github.com/BloodHoundAD/BloodHound/releases/download/v4.3.1/BloodHound-linux-x64.zip
+unzip -q BloodHound-linux-x64.zip
+sudo mv BloodHound-linux-x64 /bin/bloodhound
+cd /bin/bloodhound
+./BloodHound --no-sandbox
+# Add alias to zshrc
+echo "alias bloodhound='/bin/bloodhound/BloodHound'" >> ~/.zshrc
+#################################################################
+
+###########################DIRSEARCH#############################
+#cd ~/opt/
+#git clone https://github.com/maurosoria/dirsearch.git --depth 1
+#cd dirsearch
+#sudo pip3 install -r requirements.txt
+#echo "alias dirsearch='~/opt/dirsearch/dirsearch.py'" >> ~/.zshrc
+sudo apt install dirsearch #deprecated but still working 11/05/2024
+
+# Ip fix
+#sudo ifconfig eth0 192.168.226.129 netmask 255.255.255.0 broadcast 192.168.226.255
+#sudo ip link set dev eth0 down
+#sudo ip link set dev eth0 up
+#sudo systemctl restart networking
+
+
+#Install Powershell on Kali
+###################################
+sudo apt-get update
+sudo apt-get install -y wget
+source /etc/os-release
+wget -q https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
+pwsh
+exit
+#Error in Kali Linux instead use 6.0
+sudo apt install dotnet-runtime-deps-7.0
+sudo apt install mono-complete
+###################################
+
 # Permisos para el usuario kali en el archivo target
 sudo chown kali:kali /home/kali/.config/bin/target
 
@@ -45,3 +97,5 @@ notify-send "Installation complete. Your environment is ready."
 
 # Exit the script
 exit 0
+
+
